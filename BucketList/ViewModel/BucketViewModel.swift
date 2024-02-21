3675efb5f02e63df0b4683de0b3ae48b27cab567
PochiApp <pochi.app.dp@gmail.com>
2024-02-21 22:58:18 +0900

    アプリ起動時すぐの画像保存一応完成

--- BucketList/ViewModel/BucketViewModel.swift
+++ BucketList/ViewModel/BucketViewModel.swift
@@ -210,16 +210,21 @@ class BucketViewModel : ObservableObject{
     
     func convertDataimages (photos: [PhotosPickerItem]) async {
         
+    var newDatas: [Data] = []
+        
         for photo in photos {
-                
-                guard let data = try? await photo.loadTransferable(type: Data.self) else { continue }
+            guard let data = try? await photo.loadTransferable(type: Data.self) else { continue }
+            newDatas.append(data)
+        }
+        
+        let appendData = Array(Set(newDatas))
+        
             DispatchQueue.main.async {
-                print("最初\(data)")
-                
-                self.datas.append(data)
+                print("最初\(appendData.count)")
+                self.datas.append(contentsOf: appendData)
+                print("3")
             }
             
-        }
         
         
     }
@@ -279,4 +284,7 @@ class BucketViewModel : ObservableObject{
             print("カテゴリー追加できませんでした")
         }
     }
+    
+    
 }
+
