49ab047f14ebaab48c065007cf89b4bb6a52ae75
ユーザー名 <pochi.app.dp@gmail.com>
2024-04-18 20:14:34 +0900

    Admob用 Info.plistへKey追加、SDK初期化

--- WishList/WishListApp.swift
+++ WishList/WishListApp.swift
@@ -7,9 +7,21 @@
 
 import SwiftUI
 import CoreData
+import GoogleMobileAds
 
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        GADMobileAds.sharedInstance().start(completionHandler: nil)
+        
+        return true
+    }
+}
 @main
 struct WishListApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     private let persistence = PersistenceController.shared
         var body: some Scene {
             WindowGroup {
