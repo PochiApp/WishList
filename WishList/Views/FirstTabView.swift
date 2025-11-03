5229a29e444e1f9055200b3fe4dc1c0c3915279f
swift <pochi.app.dp@gmail.com>
2025-11-03 11:19:58 +0900

    不要なViewModelの作成を削除

--- WishList/Views/FirstTabView.swift
+++ WishList/Views/FirstTabView.swift
@@ -8,9 +8,6 @@
 import SwiftUI
 
 struct FirstTabView: View {
-    //ViewModelのインスタンス作成 これを各Viewに引数で渡してViewModelにアクセスできるようにする
-    @StateObject var wishListViewModel = WishListViewModel()
-    
     init() {
         //TabBarの背景色を白に変更
         UITabBar.appearance().backgroundColor = .white
@@ -18,7 +15,7 @@ struct FirstTabView: View {
     
     var body: some View {
         TabView {
-            FolderView(wishListViewModel: wishListViewModel)
+            FolderView()
                 .tabItem {
                     Label("フォルダー", systemImage: "folder")
                 }
